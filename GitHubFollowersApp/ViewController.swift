9380f991bff92ce80e794d16f01fc570b7cb6bfc
Diego Ferreira <diferodrigues@gmail.com>
2022-07-19 15:16:51 -0300

    remove storyboard with window scene

---
+++ GitHubFollowersApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GitHubFollowersApp
+//
+//  Created by Diego Ferreira on 19/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
+
